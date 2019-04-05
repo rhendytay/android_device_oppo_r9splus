bb9aeede2ea87114e191a17dff8fe6bed3915d23
Rhendy Tay <rhendytay@gmail.com>
2019-04-05 10:00:41 +0700

    CM-14.1 For OPPO R9s Plus

---
+++ kernel/user/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Nov 10 17:59:05 CST 2016"
+#define LINUX_COMPILE_BY "wuxianlin"
+#define LINUX_COMPILE_HOST "wuxianlin-ubuntu"
+#define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (GCC) "
---
+++ kernel/user/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.84-perf-geb8d45a"
